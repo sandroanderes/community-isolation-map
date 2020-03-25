72438eaa636630db16a6aa072e395aa3ad050aba
Sandro Anderes <sandroanderes@outlook.com>
2020-03-25 20:51:34 +0100

    updated title and categories

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,6 +1,5 @@
 module.exports = [
 	{ ident: 'childcare', text: 'Kinderhort', color: '#1289A7' },
-	{ ident: 'safespace', text: 'Communal Safe Space', color: '#7f8c8d' },
 	{ ident: 'food', text: 'Essen und Trinken', color: '#D980FA' },
 	{ ident: 'shop', text: 'Einkaufszentrum', color: '#8e44ad' },
 	{ ident: 'information', text: 'Information', color: '#2980b9' },
--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const Index = () => {
           <div className='ui container'>
             <h1 className='ui header'>
               <div className='content'>
-                <span className={'page-title'}>Community Isolation Map: The information network to keep your community safe.</span>
+                <span className={'page-title'}>Community Isolation Map of Aarau: The information network to keep your community safe.</span>
                 <div className='sub header'>
                   Check out the interactive map and stay in touch with the community during the coronavirus isolation.{' '}
                   <Link to={'/add'}>
@@ -37,9 +37,6 @@ const Index = () => {
                 </div>
               </div>
             </h1>
-            <div className={'ui yellow message'} style={{fontWeight: 500, fontStyle: 'italic'}}>
-              <i className={'icon external link'} />Read the Tutorial about how to set up your own map <a href={'https://medium.com/@marcfehr/how-to-build-a-fast-and-reliable-community-mapping-tool-with-gatsbyjs-and-firebase-74a0fa6b5b83?source=userActivityShare-f57d26da4972-1584988662&_branch_match_id=689400773593121406'} target={'_blank'} rel={'noopener noreferrer'}>here</a>.
-            </div>
           </div>
         </section>
         <section>
