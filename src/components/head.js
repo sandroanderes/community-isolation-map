65d4282d61d7b582487afa08a763989ae214520d
Sandro Anderes <sandroanderes@outlook.com>
2020-03-30 09:30:40 +0200

    updated meta tags

--- src/components/head.js
+++ src/components/head.js
@@ -20,12 +20,12 @@ const Head = ({ title }) => {
       title={`${title} | ${data.site.siteMetadata.title}`}
       meta={[
         { name: 'description', content: data.site.siteMetadata.description },
-        { name: 'og:url', content: "https://www.whozinberg.org" },
+        { name: 'og:url', content: "https://www.zaemestarch.ch" },
         { name: 'og:image', content: data.site.siteMetadata.teaserImage },
         { name: 'og:image:secure_url', content: data.site.siteMetadata.teaserImage },
         { name: 'og:locale', content: "de_CH"},
         { name: 'og:image:type', content: "image/jpeg"},
-        { name: 'og:image:alt', content: "Whozinberg.org Teaser Image with photo taken from above." },
+        { name: 'og:image:alt', content: "zaemestarch.ch Teaser Image with photo taken from above." },
       ]}
     />
   );
